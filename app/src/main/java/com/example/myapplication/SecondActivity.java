9fedadf0b7cf74c2a00608e3f9995af816cb692c
jordytos <jordynabina@gmail.com>
2021-02-23 18:11:41 +0100

    commit 2

---
+++ app/src/main/java/com/example/myapplication/SecondActivity.java
@@ -0,0 +1,120 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SecondActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        tv = (TextView) findViewById(R.id.textView1);
+
+
+        popUp("onCreate() activity2");
+
+    }
+    
+
+    /** =============================================================
+     * Exécuté que l'activité arrêtée via un "stop" redémarre.
+     *
+     * La fonction onRestart() est suivie de la fonction onStart().
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        popUp("onRestart() activity2");
+    }
+    /** ==============================================================
+     * Exécuté lorsque l'activité devient visible à l'utilisateur.
+     *
+     * La fonction onStart() est suivie de la fonction onResume().
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        /*SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        setTxTValeur(settings.getString("valeur", ""));*/
+        popUp("onStart() activity2");
+    }
+    /** ==============================================================
+     * Exécutée à chaque passage en premier plan de l'activité.
+     * Ou bien, si l'activité passe à nouveau en premier
+     * (si une autre activité était passée en premier plan entre temps).
+     *
+     * La fonction onResume() est suivie de l'exécution de l'activité.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        tv.setText("Bienvenue dans l'activité 2 "+settings.getString("valeur", " "));
+        popUp("onResume() activity2");
+    }
+    /** =============================================================
+     * La fonction onPause() est suivie :
+     * - d'un onResume() si l'activité passe à nouveau en premier plan
+     * - d'un onStop() si elle devient invisible à l'utilisateur
+     *
+     * L'exécution de la fonction onPause() doit être rapide,
+     * car la prochaine activité ne démarrera pas tant que l'exécution
+     * de la fonction onPause() n'est pas terminée.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        /*SharedPreferences settings = getSharedPreferences("cycle_vie_prefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("valeur", getTxtValeur());
+        editor.commit();
+
+        if (isFinishing()) {
+            popUp("onPause, l'utilisateur à demandé la fermeture via un finish()");
+        } else {
+            popUp("onPause, l'utilisateur n'a pas demandé la fermeture via un finish()");
+        }*/
+    }
+    /** ==============================================================
+     * La fonction onStop() est exécutée :
+     * - lorsque l'activité n'est plus en premier plan
+     * - ou bien lorsque l'activité va être détruite
+     *
+     * Cette fonction est suivie :
+     * - de la fonction onRestart() si l'activité passe à nouveau en premier plan
+     * - de la fonction onDestroy() lorsque l'activité se termine
+     *    ou bien lorsque le système décide de l'arrêter
+     */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        popUp("onStop() activity2");
+    }
+    /** =============================================================
+     * Cette fonction est exécutée lorsque l'activité se termine ou bien lorsque
+     * le système décide de l'arrêter.
+     *
+     * La fonction onCreate() devra à nouveau être exécutée pour obtenir à nouveau l'activité.
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        popUp("onDestroy() activity2");
+    }
+    //=================================================================
+
+    public void popUp(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+}
